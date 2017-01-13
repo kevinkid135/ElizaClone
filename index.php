f142ec0a354f8e84f2e432af95f65d9b29dcd53c
kevinkid135 <kevinkid228@hotmail.com>
2017-01-13 00:35:45 -0500

    Initial commit Basic UI set up

---
+++ index.php
@@ -0,0 +1,17 @@
+<head>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<h1>Eliza Clone</h1>
+<div id="messages">
+    <div class="elizaMsg">Hello. How are you feeling today?</div>
+    <!--    <div class="userMsg">testing</div>-->
+    <div class="loading">Typing</div>
+</div>
+<div id="userInputContainer">
+    <input id="userInput" autofocus>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="main.js"></script>
+</body>
+
---
+++ main.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Kevin on 01/12/2017.
+ */
+document.getElementById("userInput")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            var inputField = $('#userInput');
+            var input = inputField.val();
+
+            // print msg and clear input field
+            printMsg("userMsg", input);
+            inputField.val("");
+
+            // Eliza Respond
+            $('.loading').show("fast", "swing"); // typing animation
+            setTimeout(
+                function () {
+                    return respond(input);
+                }, 1000);
+        }
+    });
+
+/**
+ * Algorithm for Eliza's response
+ * @param userInput
+ */
+function respond(userInput) {
+    userInput = userInput.toLowerCase();
+    // TODO create algorithm to respond to user
+
+    var reply = "Actually, I have something to do... bye.";
+    $('.loading').hide("fast", "swing");
+    printMsg("elizaMsg", reply);
+}
+
+/**
+ * Modifiest the DOM element to print message to screen
+ * @param msgOwner elizaMsg | userMsg
+ * @param msg message to be printed
+ */
+function printMsg(msgOwner, msg) {
+    var msgElement = $('#messages');
+    msgElement.append('<div class="' + msgOwner + '">' + msg + '</div>');
+    msgElement.scrollTop(msgElement.prop("scrollHeight"));
+}
